2f187514053227aeccef53d7f6b9792459f0f94d
Enes T <66560757+devenes@users.noreply.github.com>
2022-03-05 11:58:11 +0300

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Kaç tane variable tanımlanacağı kullanıcıdan sayı input olarak alın.
-read -p "Kaç tane variable tanımlanacağını giriniz: " var_sayi
+# Kaç tane variable tanımlanacağını kullanıcıdan sayı input olarak alın.
+read -p "Kaç tane cümle tanımlamak istediğinizi giriniz: " var_sayi
 
 # Variableları dosya içerisine yazın.
 for i in $(seq 1 $var_sayi); do
-    read -p "Cümlenizi giriniz: " var_adi
+    read -p "Kaydetmek istediğiniz cümleyi giriniz: " var_adi
     echo "$var_adi" >> variable.txt
 done
 
 # Kullanıcıdan gösterilecek satır ve kelimeyi sayı variable olarak alın.
 read -p "Gösterilecek satır numarasını giriniz: " gosterilecek_satir
-read -p "Gösterilecek kelime numarasını giriniz: " gosterilecek_kelime
+read -p "Seçtiğiniz satır içerisinde gösterilecek kelimenin sıra numarasını giriniz: " gosterilecek_kelime
 
 # Alınan variableları kullanarak dosya içerisinde seçilen satırla seçilen kelimeyi ekrana yazdırın.
 echo "Görmek istediğiniz satır: $(sed -n "$gosterilecek_satir"p variable.txt) "
